7f340a71a540925111df989acdca019db30f99c1
Joshua Sianquita <siankeets@gmail.com>
2026-06-09 12:20:17 +0800

    fully working now hehe

--- rps.py
+++ rps.py
@@ -1,9 +1,9 @@
 # ROCK PAPER SCISSORS
 # Menu
-#---- user pick an option
-#---- system randomnly picks option
-#---- fight rock>scissor>paper>rock
-#---- history of matches (maybe in txt file)
+#---- user pick an option  / 
+#---- system randomnly picks option   / 
+#---- fight rock>scissor>paper>rock    / 
+#---- history of matches (maybe in txt file)  
 #---- use machine learning maybe for prediction of next system move  
 #---- create statistic for % of picked options
 
@@ -13,30 +13,35 @@ startChecker = False
 user_option = ""
 bot_option = ""
 options = ("ROCK", "PAPER", "SCISSOR")
-
-
+startChecker2 = True
+num = 1
 
 class func():
     
     def invalidmsg(self):
         print(" ")
         print("This option is not valid, to enter the fight, please type the right option!")
-        print("ROCK   /  PAPER  /  SCISSOR")
-        print("=" * 50)
+        print("                     ROCK   /  PAPER  /  SCISSOR")
+        print(" ")
+        
+    def invalidmsg2(self):
+        print(" ")
+        print("This option is not available, please type Y or N!")
+        print(" ")
         
     def exitmsg(self):
         print(" ")
-        print("Thank you for using the app!")
-        print("Exiting....")
+        print("         Thank you for using the app!")
+        print("                 Exiting....")
         print("=" * 50)
 
     def conmsg(self):
+        print(" ")
         print("Would you like to play again? Y/N")
         answer = input(": ")
         answer = answer.upper()
         answer = answer.strip()
         return answer
-        
 
     def resultMaker(self, u_option, b_option):
         try:
@@ -53,8 +58,6 @@ class func():
         except:
             invalidmsg()
 
-
-
     def rock(self, u_option, b_option):
         try:
             if u_option == "ROCK" and b_option == "SCISSOR":
@@ -67,8 +70,6 @@ class func():
                 print("You lost...")
         except:
             invalidmsg()
-
-
 
     def scissor(self, u_option, b_option):
         try:
@@ -84,7 +85,6 @@ class func():
         except:
             invalidmsg()
 
-
     def paper(self, u_option, b_option):
         try:
             if u_option == "PAPER" and b_option == "ROCK":
@@ -99,60 +99,59 @@ class func():
         except:
             invalidmsg()
             
-            
-
-    def main(self, checker):
+    def main(self, checker, num):
         while checker:
             print("=" * 50)
             print(" ")
-            print("     PICK YOUR FIGHTER!      ")
-            print(" ROCK   /  PAPER  /  SCISSOR ")
+            
+            print(f"              ROUND {num} ")
+            print("         PICK YOUR FIGHTER!      ")
+            print("    ROCK   /  PAPER  /  SCISSOR ")
+            print(" ")
             user_option = input (": ")
             user_option = user_option.upper()
             user_option = user_option.strip()
             bot_option = random.choice(options)
             
             if user_option in options:
-                print(f"Your fighter: {user_option}")
-                print(f"Bot's fighter: {bot_option}")
+                print(f"Your fighter:   {user_option}")
+                print(f"Bot's fighter:  {bot_option}")
                 print(" ")
                 self.resultMaker(user_option, bot_option)
                 
-                self.conmsg()
-                print(" ")
-                if self.conmsg == "Y":
-                    continue
-                elif self.conmsg == "N":
-                    self.exitmsg()
-                    exit
-                else:
-                    self.invalidmsg()
-                        
+                checker2 = True
+                while checker2:
+                    x = self.conmsg()
+                    print(" ")
+                    if x == "Y":
+                        checker2 = False
+                        num += 1
+                        break
+                    elif x == "N":
+                        self.exitmsg()
+                        exit()
+                    else:
+                        self.invalidmsg2()
+                
             else:
                 self.invalidmsg()
-        
-        
-
+    
 #MAIN
-
 f = func()
-
-print("=" * 50)
-print(" ")
-print("         PLAY ROCK, PAPER, SCISSORS!     ")
-startup = input("       Would you like to play? (Y/N): ")
-answer = startup.upper()
-answer = answer.strip()
-
-if answer == "Y":
-    startChecker = True
-    f.main(startChecker)
-    
-elif answer == "N":
-    f.exitmsg()
-    exit
-else:
-    f.invalidmsg()
-    
-    
-    
+while startChecker2:
+    print("=" * 50)
+    print(" ")
+    print("         PLAY ROCK, PAPER, SCISSORS!     ")
+    startup = input("       Would you like to play? (Y/N): ")
+    print(" ")
+    answer = startup.upper()
+    answer = answer.strip()
+    if answer == "Y":
+        startChecker = True
+        f.main(startChecker, num)
+        
+    elif answer == "N":
+        f.exitmsg()
+        startChecker2 = False
+    else:
+        f.invalidmsg2()
